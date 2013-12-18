3f230a46bda9cc6b455809726a79a74046b9eece
Jie Yang <outstanding.yang@gmail.com>
2013-12-18 16:20:48 +0800

    Use clang complete for c/c++ completion

--- lisp/init-auto-complete.el
+++ lisp/init-auto-complete.el
@@ -1,14 +1,21 @@
 (require-package 'auto-complete)
+(require-package 'auto-complete-clang)  ;clang package for c/c++ completion
 (require 'auto-complete-config)
 (global-auto-complete-mode t)
 (setq-default ac-expand-on-auto-complete nil)
 (setq-default ac-auto-start nil)
 (setq-default ac-dwim nil) ; To get pop-ups with docs even if a word is uniquely completed
+(ac-set-trigger-key "TAB")              ;AFTER input prefix, press TAB key ASAP
+
+;; Use C-n/C-p to select candidates ONLY when completion menu is displayed
+(setq ac-use-menu-map t)
+(define-key ac-menu-map "\C-n" 'ac-next)
+(define-key ac-menu-map "\C-p" 'ac-previous)
 
 ;;----------------------------------------------------------------------------
 ;; Use Emacs' built-in TAB completion hooks to trigger AC (Emacs >= 23.2)
 ;;----------------------------------------------------------------------------
-(setq tab-always-indent 'complete)  ;; use 't when auto-complete is disabled
+;;(setq tab-always-indent 'complete)  ;; DO NOT use 't when auto-complete is disabled
 (add-to-list 'completion-styles 'initials t)
 
 ;; TODO: find solution for php, c++, haskell modes where TAB always does something
@@ -34,6 +41,7 @@
 (set-default 'ac-sources
              '(ac-source-imenu
                ac-source-dictionary
+               ac-source-yasnippet
                ac-source-words-in-buffer
                ac-source-words-in-same-mode-buffers
                ac-source-words-in-all-buffer))
@@ -46,9 +54,16 @@
                 lisp-mode textile-mode markdown-mode tuareg-mode
                 js3-mode css-mode less-css-mode sql-mode
                 sql-interactive-mode
-                inferior-emacs-lisp-mode))
+                inferior-emacs-lisp-mode c-mode c++-mode))
   (add-to-list 'ac-modes mode))
 
+;; clang stuff
+;; @see https://github.com/brianjcj/auto-complete-clang
+(defun my-ac-cc-mode-setup ()
+  (require 'auto-complete-clang)
+  (setq ac-sources (append '(ac-source-clang) ac-sources)))
+(add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
+(add-hook 'auto-complete-mode-hook 'ac-common-setup)
 
 ;; Exclude very large buffers from dabbrev
 (defun sanityinc/dabbrev-friend-buffer (other-buffer)
