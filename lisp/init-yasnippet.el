46745fefb08d434e68458ca32291b7e68b73a63e
Jie Yang <outstanding.yang@gmail.com>
2014-06-13 14:44:37 +0800

    Set up mySnippets for yas expand

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,6 +1,8 @@
 ;;; Yet another snippet extension for Emacs config
 (require-package 'yasnippet)
 (require 'yasnippet)
+(setq yas-snippet-dirs
+      '("~/.emacs/mySnippets"))         ; Personal snippets
 (yas-global-mode 1)
 
 (provide 'init-yasnippet)
